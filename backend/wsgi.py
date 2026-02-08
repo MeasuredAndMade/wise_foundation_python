07e52f58bc76cd5063e2e1106532e70bbb0dbe26
Measured & Made <measuredandmade@outlook.com>
2026-02-08 02:37:06 -0500

    I'm Fucking Sick of this!

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,18 +1,10 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-from .create_superuser import run as create_superuser
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+# Import AFTER Django is fully loaded
+from .create_superuser import run as create_superuser
 create_superuser()
